bb90f68940585d4aa16442119122e5536ef8c5cd
LaelatulHidayah <laelatulhidayahela@gmail.com>
2023-01-19 08:53:49 +0700

    Tajwid NGALAH

---
+++ app/src/main/java/com/ella/reycicle/MainActivity.java
@@ -0,0 +1,108 @@
+package com.ella.reycicle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter recyclerViewAdapter;
+    RecyclerView.LayoutManager recyclerViewLayoutManager;
+    ArrayList<ItemModel> data;
+
+    private Button profilButton;
+    private Button sanadButton;
+
+    MediaPlayer player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        profilButton = findViewById(R.id.profil);
+        sanadButton = findViewById(R.id.sanad);
+
+        profilButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, profil.class);
+                startActivity(intent);
+            }
+        });
+
+        sanadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, sanad.class);
+                startActivity(intent);
+            }
+        });
+
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerViewLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(recyclerViewLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
+        data = new ArrayList<>();
+        for (int i = 0; i < myitem.Headline.length; i ++) {
+            data.add(new ItemModel(
+                    myitem.Headline[i],
+                    myitem.Subheadline[i],
+                    myitem.iconlist[i]));
+        }
+
+        recyclerViewAdapter = new AdapterRecyclerView(this, data);
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+    public void play(View v) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.makhrotan);
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlayer();
+                }
+            });
+
+        }
+        player.start();
+    }
+
+    public void pause(View v) {
+        if (player != null){
+            player.pause();
+        }
+    }
+
+    public void stop(View v) {
+        stopPlayer();
+    }
+
+    private void stopPlayer() {
+        if (player != null){
+            player.release();
+            player =null;
+        }
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        stopPlayer();
+    }
+
+}
