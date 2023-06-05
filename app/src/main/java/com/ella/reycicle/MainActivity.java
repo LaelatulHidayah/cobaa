61da9baa7c369e309de37546aa3e41fd8fc22b8c
LaelatulHidayah <laelatulhidayahela@gmail.com>
2023-06-05 07:36:27 +0700

    aplikasi tes kenaikan jenjang MQ Ngalah

--- app/src/main/java/com/ella/reycicle/MainActivity.java
+++ app/src/main/java/com/ella/reycicle/MainActivity.java
@@ -68,41 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerViewAdapter = new AdapterRecyclerView(this, data);
         recyclerView.setAdapter(recyclerViewAdapter);
-    }
-    public void play(View v) {
-        if (player == null) {
-            player = MediaPlayer.create(this, R.raw.makhrotan);
-            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mp) {
-                    stopPlayer();
-                }
-            });
 
-        }
-        player.start();
-    }
 
-    public void pause(View v) {
-        if (player != null){
-            player.pause();
-        }
     }
-
-    public void stop(View v) {
-        stopPlayer();
-    }
-
-    private void stopPlayer() {
-        if (player != null){
-            player.release();
-            player =null;
-        }
-    }
-    @Override
-    protected void onStop(){
-        super.onStop();
-        stopPlayer();
-    }
-
 }
